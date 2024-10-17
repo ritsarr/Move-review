7d233bae9d8e2f9a5908c9af97810f5eaeafb7c6
Daniil Morozov <164764438+ritsarr@users.noreply.github.com>
2024-10-18 02:13:14 +0300

    Improved

--- movie_review_analyzer/analyzer_app/forms.py
+++ movie_review_analyzer/analyzer_app/forms.py
@@ -2,7 +2,6 @@ from django import forms
 
 class ReviewForm(forms.Form):
     review_text = forms.CharField(
-        label='Введите Ваш отзыв:',
+        label='Введите ваш отзыв',
         widget=forms.Textarea(attrs={'rows': 5, 'cols': 40})
     )
-
--- movie_review_analyzer/sentiment_model/model.py
+++ movie_review_analyzer/sentiment_model/model.py
@@ -1,31 +1,43 @@
 import torch
-import torch.nn as nn
 from transformers import BertTokenizer, BertModel
+import torch.nn as nn
+
+device = torch.device('cpu')
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+MAX_LENGTH = 128
 
 class SentimentRatingModel(nn.Module):
-    def __init__(self, n_ratings):
+    def __init__(self):
         super(SentimentRatingModel, self).__init__()
         self.bert = BertModel.from_pretrained('bert-base-uncased')
         self.dropout = nn.Dropout(p=0.3)
-        self.sentiment_classifier = nn.Linear(self.bert.config.hidden_size, 2)
-        self.rating_classifier = nn.Linear(self.bert.config.hidden_size, n_ratings)
+        self.sentiment_classifier = nn.Sequential(
+            nn.Linear(self.bert.config.hidden_size, 256),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(256, 2)
+        )
+        self.rating_regressor = nn.Sequential(
+            nn.Linear(self.bert.config.hidden_size, 256),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(256, 1)
+        )
 
     def forward(self, input_ids, attention_mask):
         outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask)
-        pooled_output = outputs[1]
+        pooled_output = outputs.pooler_output
         pooled_output = self.dropout(pooled_output)
         sentiment_logits = self.sentiment_classifier(pooled_output)
-        rating_logits = self.rating_classifier(pooled_output)
-        return sentiment_logits, rating_logits
+        rating_output = self.rating_regressor(pooled_output).squeeze(-1)
+        return sentiment_logits, rating_output
 
-def load_model(device):
-    model = SentimentRatingModel(n_ratings=10)
-    model.load_state_dict(torch.load('sentiment_model/sentiment_rating_model.pth', map_location=device))
-    model.to(device)
+def load_model():
+    model = SentimentRatingModel()
+    model.load_state_dict(torch.load('sentiment_model/sentiment_rating_model_final.pth', map_location=device))
     model.eval()
+    model.to(device)
     return model
 
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-MAX_LENGTH = 256
-
-
+model = load_model()
